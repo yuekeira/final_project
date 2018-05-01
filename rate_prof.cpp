9b7f5c7a5ba2d9782c916ee5a835a26e4a087627
yuekeira <yuekeira@gmail.com>
2018-05-01 13:37:22 -0400

    keira

--- rate_prof.cpp
+++ rate_prof.cpp
@@ -1,64 +1,233 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
+#include <set>
 using namespace std;
+
+int status;
+int input;
+string dept_name;
 
 class Professor {
 
 public:
     string name;
     double Diff_level;
-    string dept;
+//    string dept;
     double new_Diff_level;
-    Professor(string str, double dl, string dept) : name(str), Diff_level(dl), dept(dept) {}
+    int student_num;
+    string dept_name;
+    Professor(string str, double dl, int student, string dept) : name(str), Diff_level(dl), student_num(student), dept_name(dept) {}
 
-    void changeDiff(double new_Diff_level, double student_num) {Diff_level = (Diff_level+new_Diff_level)/student_num;}
+    void changeDiff(double new_Diff_level,int student_num) {Diff_level = (Diff_level*(student_num-1)+new_Diff_level)/student_num;}
+    void changenumber(int num) {student_num = num + 1;}
+    friend bool operator< (const Professor &lhs, const Professor &rhs);
 };
 
-//class MEProfessor : public Professor {
-//public:
-//    MEProfessor(string str) : Professor(str) {}
-//};
-//
-//class PhysicsProfessor : public Professor {
-//public:
-//    bool monopoly;
-//   PhysicsProfessor(string str) : Professor(str) {}
-//};
+bool operator< (const Professor &lhs, const Professor &rhs)
+{
+	if (lhs.Diff_level < rhs.Diff_level)
+		return true;
+	else if (lhs.Diff_level > rhs.Diff_level)
+		return false;
+	else //compare name
+	{
+		if(lhs.name < rhs.name)
+			return true;
+		else if (lhs.name > lhs.name)
+			return false;
+		else 
+			return false;
+	}
+}
+
+
+	
+
+void check();
+void create();
+void rate();
+void linedel(string line2del);
+void linewrite(string name, double Diff, int student_num, string dept_name);
+void low2high();
+
+
 
 int main()
 {
-    string prof_name;
-    double Diff_level;
-    string dept;
-    int student_num = 1;
-    double new_Diff_level;
-    cout <<"Enter the last name of the professor:" ;
-    cin >> prof_name;
-    //Professor name(prof_name);
-    cout << "Please rate the level of difficulty:";
-    cin >> Diff_level;
-    cout << "Which department?";
-    cin >> dept;
-    Professor newProfessor(prof_name,Diff_level,dept);
-    cout << newProfessor.name << newProfessor.Diff_level << endl;
-
-    ofstream newfile ("cooper_prof.txt", ios::app);
-//    newfile.open("cooper_prof.txt");
-    newfile<< newProfessor.name<< " " << newProfessor.Diff_level<< " " << endl;
-//    newfile.close();
-
-
-    student_num++;
-    cout << "Enter a new rating:";
-    cin >> new_Diff_level;
-    newProfessor.changeDiff(new_Diff_level,student_num);
-    cout << "new rating is" << newProfessor.Diff_level << endl;
-//    ofstream newfile("cooper_prof.txt", ios::app);
-//    newfile.open("cooper_prof.txt");
-    newfile<<newProfessor.name << " " << newProfessor.Diff_level << " " << endl;
-    newfile.close();
-
-
+    cout << "________________________________________" << endl;
+    cout << "what would you like to do" << endl;
+    cout << "1 check a professor's rating" << endl;
+    cout << "2 rate a professor" <<endl;
+    cout << "3 create a professor" <<endl;
+    cout << "4 quit the programm" << endl;
+    cout << "5 display professor from low to high" << endl;
+    cout << " "<< endl;
+    cin >> input;
+    if (input == 1)
+            check();
+    else if (input ==2)
+            rate();
+    else if (input == 3)
+	    create();
+    else if (input == 4)
+	    exit(0);
+    else if (input == 5)
+	    low2high();
+    else
+	    cout <<"Invalid input, please enter a number from 1 to 5." << endl;
+	
     return 0;
+}
+
+
+void create()
+{
+        string name, dept_name;
+        double Diff_level;
+	int student_num = 1;
+        ofstream file("cooper_prof.txt", ios::app);
+        cout <<"Please enetr professor last name " << endl;
+	cin >> name;
+	cout << "Please enter the department (EE,ME,CE,CHE,FA,ARCH)"<<endl;
+	cin >> dept_name;
+	cout << "Please rate the level of difficulty: (scale 1 to 5)"<<endl;
+        cin >> Diff_level;
+        cout << name << " " << dept_name << " " <<Diff_level << " "<< endl;
+        file << name << " " << dept_name<< " " << Diff_level<< " " << student_num << endl;
+        file.close();
+        main();
+}
+
+
+void check()
+{
+	status = 0;
+	ifstream file("cooper_prof.txt");
+	string name, search_name;
+	int student_num;
+	char input;
+	double Diff_level;
+	cout << "enter the professor's last name"<<endl;
+	cin >> search_name;
+	while (file >> name >> dept_name >>Diff_level>> student_num)
+	{
+		if (search_name == name)
+		{
+//			cout << "professor found" << endl;
+//			cout << name << " " << Diff_level << " || Rated by " << student_num << " students."<<endl;
+			status = 1;
+			
+		}
+	}
+	cout << name << " " << Diff_level << " || Rated by " << student_num << " students."<<endl;
+	cout << " " << endl;
+	if(status == 0)
+	{
+		cout<<"prof not found"<< endl;
+		cout<<"Please create a new one" << endl;
+	}
+			
+	
+	main();
+}
+
+void rate()
+{
+	status = 0;
+	double new_Diff_level;
+	double Diff_level;
+	int student_num;
+	string name, name2rate;
+	string line2del;
+	stringstream ss;
+
+
+	ifstream file("cooper_prof.txt");
+	cout <<" enter the professor you want to rate"<<endl;
+	cin >> name2rate;
+
+	while	 (file >> name >> dept_name >> Diff_level >> student_num)
+	{
+		if (name2rate == name)
+		{
+			
+			Professor newProf(name, Diff_level, student_num,dept_name);
+			cout << newProf.name << " " << newProf.Diff_level<< endl;
+			ss << newProf.name << " " <<newProf.Diff_level<< " " <<newProf.student_num;
+			line2del = ss.str();
+			linedel(line2del);
+			cout << "enter your rating"<< endl;
+			cin >> new_Diff_level;
+			newProf.changenumber(newProf.student_num);
+		        newProf.changeDiff(new_Diff_level, newProf.student_num);
+			cout <<"new rating is: " <<newProf.Diff_level << endl;
+			linewrite(newProf.name, newProf.Diff_level, newProf.student_num, newProf.dept_name);
+			status = 1;
+		}
+	}	
+ 
+	
+	if (status = 0)
+	{
+	cout<< "prof not found, plase create new one" << endl;
+	}
+	main();
+}
+
+
+void linedel(string line2del)
+{
+    	string line;
+	ifstream fin;
+    	fin.open("cooper_prof.txt");
+   	ofstream temp;
+  	temp.open("temp.txt");
+
+    	while (getline(fin,line))
+   	{
+      		if (line != line2del)
+       		{
+       			temp << line << endl;
+       		}
+   	}	
+
+  	temp.close();
+   	fin.close();
+   	cout << "line deleted"<<endl;
+	remove("cooper_prof.txt");
+   	rename("temp.txt","cooper_prof.txt");
+}
+
+void linewrite(string name, double Diff, int student_num, string dept_name)
+{
+	ofstream file("cooper_prof.txt", ios::app);
+	file<<name<< " " << dept_name<<" "<< Diff<< " " << student_num<< endl;
+	file.close();
+}
+
+
+void low2high()
+{
+	string name;
+	double Diff_level;
+	int student_num;
+	set<Professor> ProfSet;
+	int i = 1;
+
+	
+	ifstream file;
+	file.open("cooper_prof.txt");
+	while(file >> name >> dept_name >> Diff_level >> student_num)
+	{
+		Professor nam(name, Diff_level, student_num, dept_name);
+		ProfSet.insert(nam);
+	}
+
+	for (auto it=ProfSet.begin(); it!=ProfSet.end(); ++it)
+	{
+		cout<<it->name << " " <<it->Diff_level <<endl;
+	}
+	main();
 }
